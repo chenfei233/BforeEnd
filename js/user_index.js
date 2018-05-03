3505097d92c2018e3a16f7ac0179e96d366ee0ae
chenfei233 <lanbitouni@163.com>
2018-05-03 17:44:52 +0800

    提交5.03.1
    忘了改了哪些了
    貌似文章加载和发布

---
+++ js/user_index.js
@@ -0,0 +1,19 @@
+$("#header_left_title,.user_name,.user_logout,.header_right_about").on({
+    mouseover : function(){
+        $(this).css("color","#ffc81f");
+    },mouseout : function(){
+        $(this).css("color","white");
+    }
+}) ;
+
+$(".cate").on({
+    mouseover : function(){
+        $(this).css("color","#ffc81f");
+    },mouseout : function(){
+        $(this).css("color","#777777");
+    }
+}) ;
+$(".user_so").click(function (){
+    $("#user_so").css("display","block");
+});
+
